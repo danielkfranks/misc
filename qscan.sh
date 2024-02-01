dbbefacf07fbae3bf92049f404884b5b067e95d8
danielkfranks <danielkfranks@gmail.com>
2024-01-31 22:34:59 -0800

    Update qscan.sh
    add comments for clarity

--- qscan.sh
+++ qscan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mynet=$(ip a | grep global | grep -Eo [0-9]+.[0-9]+.[0-9]+.[0-9]+/[0-9]+)
+mynet=$(ip a | grep global | grep -Eo [0-9]+.[0-9]+.[0-9]+.[0-9]+/[0-9]+) # heavily scuffed, may not work on your machine! please let me know if it doesn't
 echo "$mynet"
 nmap $@ $mynet
